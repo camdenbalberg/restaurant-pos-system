886c1e31bd0170ed1e3f646df544251464feb3ab
lf7834am <lf7834am@tamu.edu>
2024-10-23 13:38:15 -0500

    switched to use npm for vue package manager

--- panda-web-app-frontend/src/App.vue
+++ panda-web-app-frontend/src/App.vue
@@ -1,25 +1,26 @@
 <template>
-  <div id="app">
-    <MenuItems />
-  </div>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script>
-import MenuItems from './components/MenuItems.vue';
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
+  name: 'App',
   components: {
-    MenuItems,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
 
 <style>
-/* Global styles */
-body {
-  font-family: Arial, sans-serif;
-  background-color: #f4f4f4;
-  color: #333;
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
-
