057aed55560f2c5c077b9e9c5b34cad2d27652b0
eoe789 <eduardoo.enriquez789@gmail.com>
2024-10-30 02:31:12 -0500

    started look for kiosk view

---
+++ panda-web-app-frontend/src/components/Popup.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="popup">
+      <p>{{ message }}</p>
+      <button @click="$emit('close')">Close</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Popup',
+    props: {
+      message: {
+        type: String,
+        required: true,
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  .popup {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    background-color: white;
+    padding: 20px;
+    border: 1px solid black;
+  }
+  </style>
--- panda-web-app-frontend/src/views/CustomerView.vue
+++ panda-web-app-frontend/src/views/CustomerView.vue
@@ -1,19 +1,41 @@
 <!-- src/views/AboutView.vue -->
 <template>
   <div>
-    <h1>Customer View</h1>
-    <MenuItems />
+    <button @click="showPopup('bowl')">Bowl</button>
+    <button @click="showPopup('plate')">Plate</button>
+    <button @click="showPopup('biggerPlate')">Bigger Plate</button>
+    <button @click="showPopup('entree')">A La Carte Entree</button>
+    <button @click="showPopup('side')">A La Carte Side</button>
     <router-link to="/">Go to Home</router-link>
+
+    <div v-if="popupType" class="popup">
+      <p>{{ popupType }} selected</p>
+      <button @click="closePopup">Confirm</button>
+    </div>
   </div>
 </template>
 
 <script>
-import MenuItems from '../components/MenuItems.vue'; // Adjust path if necessary
+import MenuItem from '../components/MenuItem.vue'; // Adjust path if necessary
+import Popup from '../components/Popup.vue'; // Adjust path if necessary
 
 export default {
   name: 'Customer',
   components: {
-    MenuItems,
+    MenuItem,
+  },
+  data() {
+    return {
+      popupType: null,
+    };
+  },
+  methods: {
+    showPopup(type) {
+      this.popupType = type;
+    },
+    closePopup() {
+      this.popupType = null;
+    },
   },
 };
 </script>
