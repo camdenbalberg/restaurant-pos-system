f0b6e34ad174ad7575c399b3575a7324a64247d2
nathantruongtamu <156832505+nathantruongtamu@users.noreply.github.com>
2024-10-29 20:34:50 -0500

    Editing html and css styling of login page. * Don't know how to do global css styling, maybe should change to that. * Don't know where top text is coming from, that should be removed. * No connection to backend yet.

--- panda-web-app-frontend/src/components/LoginComponents.vue
+++ panda-web-app-frontend/src/components/LoginComponents.vue
@@ -10,6 +10,7 @@
       <br />
       <label for="password">Password:</label>
       <input type="password" id="login-password" v-model="password">
+      <br />
       <button @click="submitForm">Submit</button>
     </div>
 
@@ -37,6 +38,50 @@ export default {
 </script>
 
 <style scoped>
+  :root {
+    --accentColor: #dc8a78;
+    --accentColorIntense: #db5132;
+    --accentColorWeak: #edbeb4;
+    --surfaceColor: #ccd0da;
+    --backgroundColor: #eff1f5;
+    --borderColor: #dce0e8;
+    --textColor: #4c4f69;
+    --subTextColor: #5c5f77;
 
+    color: var(--textColor);
+  }
+
+  img {
+    scale: 0.75;
+  }
+
+  input {
+    background-color: var(--surfaceColor);
+    margin-top: 15px;
+    margin-bottom: 15px;
+    margin-left: 25px;
+    margin-right: 25px;
+  }
+
+  input:focus {
+    background-color: var(--accentColorWeak);
+  }
+
+  button {
+    background-color: var(--surfaceColor);
+    scale: 1;
+    transition: scale 0.5s ease;
+    transition: background-color 0.25s ease;
+  }
+
+  button:hover {
+    background-color: var(--accentColorWeak);
+    scale: 1.1;
+  }
+
+  button:active {
+    scale: 1;
+    background-color: var(--accentColor);
+  }
 </style>
 
