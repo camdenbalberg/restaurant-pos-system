58c6dbb598f79911ebfccaca4c836e128bbc2521
eoe789 <eduardoo.enriquez789@gmail.com>
2024-10-30 01:24:55 -0500

    created customer page

--- panda-web-app-frontend/src/router/index.js
+++ panda-web-app-frontend/src/router/index.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue'; // Create this component
 import AboutView from '@/views/AboutView.vue'; // Create this component
+import CustomerView from '@/views/CustomerView.vue'; // Create this component
 
 const routes = [
   {
@@ -13,6 +14,11 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView,
+  },
+  {
+    path: '/customer',
+    name: 'customer',
+    component: CustomerView,
   },
 ];
 
---
+++ panda-web-app-frontend/src/views/CustomerView.vue
@@ -0,0 +1,19 @@
+<!-- src/views/AboutView.vue -->
+<template>
+  <div>
+    <h1>Customer View</h1>
+    <MenuItems />
+    <router-link to="/">Go to Home</router-link>
+  </div>
+</template>
+
+<script>
+import MenuItems from '../components/MenuItems.vue'; // Adjust path if necessary
+
+export default {
+  name: 'Customer',
+  components: {
+    MenuItems,
+  },
+};
+</script>
