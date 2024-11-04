6c4148cccd142aa4e4591158ed29fc00dfe395ec
FrozenEarth <frozenearth3@gmail.com>
2024-11-04 17:02:33 -0600

    add meal popup on cashier screen

--- panda-web-app-frontend/src/components/CashierMenuItems.vue
+++ panda-web-app-frontend/src/components/CashierMenuItems.vue
@@ -2,57 +2,66 @@
     <div>
       <div v-if="loading">Loading...</div>
 
+      <div v-if="!loading && !menuItems.length">No menu items available.</div>
+
       <div v-if="!loading && meals.length" class="menu-category">
         <div class="menu-category-title">Meals</div>
-        <ul>
-          <li v-for="item in meals" :key="item.menu_id">
-            <button class="menu-item" id={{item.menu_id}}>{{item.menu_name}}</button>
+        <ul class="cashier-selection-list">
+          <li v-for="item in meals">
+            <button @click="showPopup(item.menu_name, ['side', 'entree'])" class="menu-item" :id="item.menu_id">{{item.menu_name}}</button>
           </li>
         </ul>
       </div>
 
       <div v-if="!loading && appetizers.length" class="menu-category">
         <div class="menu-category-title">Appetizers</div>
-        <ul>
-          <li v-for="item in appetizers" :key="item.menu_id">
-            <button class="menu-item" id={{item.menu_id}}>{{item.menu_name}}</button>
+        <ul class="cashier-selection-list">
+          <li v-for="item in appetizers">
+            <button class="menu-item" :id="item.menu_id">{{item.menu_name}}</button>
           </li>
         </ul>
       </div>
 
       <div v-if="!loading && drinks.length" class="menu-category">
         <div class="menu-category-title">Drinks</div>
-        <ul>
+        <ul class="cashier-selection-list">
           <li v-for="item in drinks" :key="item.menu_id">
-            <button class="menu-item" id={{item.menu_id}}>{{item.menu_name}}</button>
+            <button class="menu-item" :id="item.menu_id">{{item.menu_name}}</button>
           </li>
         </ul>
       </div>
-      
-      <div v-if="!loading && !menuItems.length">No menu items available.</div>
     </div>
+
+    <CashierPopup v-if="popup" :menu_item="selectedItem" :cat="mealItems" @close="closePopup" />
   </template>
   
   <script>
   import axios from 'axios';
+  import CashierPopup from './CashierPopup.vue';
   
   export default {
+    name: 'CashierMenuItems',
+    components: {
+      CashierPopup,
+    },
     data() {
       return {
-        menuItems: [],
         loading: true,
-
+        menuItems: [],
         meals: [],
-        entrees: [],
-        sides: [],
+        // entrees: [],
+        // sides: [],
         appetizers: [],
         drinks: [],
+
+        popup: false, 
+        selectedItem: "",
+        mealItems: [],
       };
     },
     mounted() {
       this.fetchMenuItems();
     },
-    name: 'CashierMenuItems',
     methods: {
       async fetchMenuItems() {
         try {
@@ -63,13 +72,25 @@
           console.error('Error fetching menu items:', error);
         } finally {
           this.meals = this.menuItems.filter(item => item.category == "meal");
-          this.entrees = this.menuItems.filter(item => item.category == "entree");
-          this.sides = this.menuItems.filter(item => item.category == "side");
+          // this.entrees = this.menuItems.filter(item => item.category == "entree");
+          // this.sides = this.menuItems.filter(item => item.category == "side");
           this.appetizers = this.menuItems.filter(item => item.category == "appetizer");
           this.drinks = this.menuItems.filter(item => item.category == "drink");
 
           this.loading = false;
         }
+      },
+
+      showPopup(menuItem, mealStructure) {
+        this.popup = true;
+        this.selectedItem = menuItem;
+        this.mealItems = mealStructure;
+      },
+
+      closePopup() {
+        this.popup = false;
+        this.selectedItem = "";
+        this.mealItems = [];
       },
     },
   };
@@ -80,18 +101,18 @@
     margin-bottom: 2em;
   }
 
-  .menu-category ul {
-    display: grid;
-    grid-template-columns: 30vh 30vh 30vh 30vh;
-    grid-auto-rows: 1fr;
-    gap: 1.5em 1em;
-  }
-
   .menu-category-title {
     text-align: left;
     margin-left: 0.5em;
     font-weight: 700;
     font-size: 150%;
+  }
+
+  .cashier-selection-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-auto-rows: 1fr;
+    gap: 1.5em 1em;
   }
 
   .menu-item {
---
+++ panda-web-app-frontend/src/components/CashierSelection.vue
@@ -0,0 +1,90 @@
+<template>
+    <div v-if="loading">Loading...</div>
+
+    <div v-if="!loading && !filteredMenuItems.length">No menu items available.</div>
+
+    <div v-if="!loading && filteredMenuItems.length" class="cashier-selection">
+      <div class="cashier-selection-category">{{category}}</div>
+      <ul class="cashier-selection-list">
+        <li v-for="item in filteredMenuItems">
+          <button class="menu-item" :id="item.menu_id">{{item.menu_name}}</button>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'CashierSelection',
+    props: {
+      category: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        items: [],
+        loading: true,
+      };
+    },
+    computed: {
+      filteredMenuItems() {
+        return this.items.filter(item => item.category === this.category);
+      },
+    },
+    mounted() {
+      this.fetchMenuItems();
+    },
+    methods: {
+      async fetchMenuItems() {
+        try {
+          // Example usage of env var
+          console.log(import.meta.env.VITE_API_BACKEND_URL);
+          const response = await axios.get('/api/v1/menu_items');
+          this.items = response.data;
+        } catch (error) {
+          console.error('Error fetching menu items:', error);
+        } finally {
+          this.loading = false;
+        }
+      },
+    },
+  };
+  </script>
+  
+<style scoped>
+  .cashier-selection {
+    width: 80%;
+    margin-bottom: 2em;
+  }
+
+  .cashier-selection-category {
+    margin-left: 0.5em;
+    margin-bottom: 0.5em;
+    text-align: left;
+    font-weight: 600;
+    font-size: 125%;
+    text-transform: capitalize;
+  }
+
+  .cashier-selection-list {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-auto-rows: 1fr;
+    gap: 1.5em 1em;
+  }
+
+  .menu-item {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0.5em;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    background-color: #f9f9f9;
+  }
+
+</style>
