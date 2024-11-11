777206b6999f646c4f4a5fceb729feff783de0c2
leoturtle <lfexp35@gmail.com>
2024-11-10 19:05:51 -0600

    Update index.js

--- production/frontend_edits/index.js
+++ production/frontend_edits/index.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: "https://backend-12-team-12-699b369e6ba9.herokuapp.com/"
+  baseURL: "https://backend-12-team-12-699b369e6ba9.herokuapp.com/api/v1"
 });
 
 console.log("Base URL:", api.defaults.baseURL); // Check the base URL
