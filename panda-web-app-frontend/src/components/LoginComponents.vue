90e164f16163e05e5c20bbc97f8fdb7ecc7938dd
lf7834am <lf7834am@tamu.edu>
2024-11-20 16:02:56 -0600

    fancy button, animation not working

--- panda-web-app-frontend/src/components/LoginComponents.vue
+++ panda-web-app-frontend/src/components/LoginComponents.vue
@@ -10,14 +10,33 @@
       <label for="password">Password:</label>
       <input type="password" id="login-password" v-model="password">
       <br id="bigBr">
-      <button @click="submitForm">
+      <button id="submit_login_button" @click="submitForm">
         {{ buttonText }}
       </button>
     </div>
   </div>
   <div>
-    <button id="google-signin-btn" @click="googleLogin">
-      Sign in with Google
+    <button
+      id="google_login_button"
+      @click="googleLogin"
+      :class="{ 'clicked': isClicked }"
+      type="button"
+      class="text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#4285F4]/55 me-2 mb-2"
+    >
+      <svg
+        class="w-8 h-8 me-4"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 18 19"
+      >
+        <path
+          fill-rule="evenodd"
+          d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
+          clip-rule="evenodd"
+        />
+      </svg>
+      Sign in With Google
     </button>
   </div>
 </template>
@@ -35,6 +54,15 @@ export default {
   },
   methods: {
     googleLogin() {
+      this.isClicked = true;
+      setTimeout(() => {
+        this.isClicked = false;
+      }, 1000); // Animation duration (1 second)
+
+      // Call the original googleLogin method
+      this.googleInitiateFlow();
+    },
+    googleInitiateFlow() {
       window.location.href = 'http://localhost/auth/google_oauth2';
     },
     async submitForm() {
@@ -92,12 +120,37 @@ input {
   padding: 10px;
 }
 
-button {
+#submit_login_button {
   margin-top: 70px;
   font-size: 30px;
   padding-top: 25px;
   padding-bottom: 25px;
   padding-right: 50px;
   padding-left: 50px;
+}
+
+#google_login_button {
+  margin-top: 70px;
+  font-size: 30px;
+  padding-top: 25px;
+  padding-bottom: 25px;
+  padding-right: 50px;
+  padding-left: 50px;
+}
+
+.clicked {
+  animation: color-change 1s forwards;
+}
+
+@keyframes color-change {
+  0% {
+    background-color: #4285f4;
+  }
+  50% {
+    background-color: #34a853;
+  }
+  100% {
+    background-color: #ea4335;
+  }
 }
 </style>
